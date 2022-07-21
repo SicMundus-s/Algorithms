4b3e5b07df10e153228805228f876ec0cd604bf8
NikitaV <nikita.volkov0505@mail.ru>
2022-07-22 00:50:06 +0300

    Basic model programming added main

---
+++ Basic/src/BasicModelProgramming/Main.java
@@ -0,0 +1,9 @@
+package BasicModelProgramming;
+
+
+//Main будет получать объекты классов с реализацией алгоритмов и тестировать их с разными значениями
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
