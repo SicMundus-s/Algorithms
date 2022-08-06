35bc603fbf8849dc8d20a864246ab509131acf8c
NikitaV <nikita.volkov0505@mail.ru>
2022-08-06 15:23:32 +0300

    Array: Binary Search

---
+++ Basic/src/Arrays/Main.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String args[]) {
+
+        int[] a = {11,22,33,44,55,66,77,88,99,100};
+        System.out.println(BinarySearch.Search(a, 5));
+    }
+}
