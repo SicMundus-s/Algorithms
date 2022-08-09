265751fda5d68b3dbec63e92db6dd197273d4781
NikitaV <nikita.volkov0505@mail.ru>
2022-08-09 13:31:19 +0300

    Buuble Sorted

---
+++ Basic/src/Sorted/Main.java
@@ -0,0 +1,13 @@
+package Sorted;
+
+import Arrays.BinarySearch;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String args[]) {
+        int[] array = {10,9,8,7,6,5,4,3,2,1};
+        BubbleSorted.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
