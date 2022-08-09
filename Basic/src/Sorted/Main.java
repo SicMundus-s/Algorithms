a77037c776cb7e4d6227c6bc8b87d25e2220f306
NikitaV <nikita.volkov0505@mail.ru>
2022-08-09 16:31:36 +0300

    Stack, Queue and PriorityQueue

--- Basic/src/Sorted/Main.java
+++ Basic/src/Sorted/Main.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String args[]) {
         int[] array = {10,9,8,7,6,5,4,3,2,1};
-        BubbleSorted.sort(array);
+        Sorted.bubbleSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
